ef69477b0dd9e531c9f70e86b5a3c968121c671b
Martien Dermawan Tanama <martiendt@gmail.com>
2024-08-22 09:30:23 +0700

    fix password reset

--- src/modules/master/views/user/index.vue
+++ src/modules/master/views/user/index.vue
@@ -42,12 +42,12 @@
               <td class="basic-table-body">{{ user.email }}</td>
               <td class="basic-table-body">{{ user.role }}</td>
               <td class="basic-table-body flex gap-2">
-                <router-link
+                <!-- <router-link
                   :to="`/master/user/${user._id}/edit`"
                   class="btn btn-sm rounded bg-blue-500 text-xs text-white"
                 >
                   Reset Password
-                </router-link>
+                </router-link> -->
                 <button
                   class="btn btn-sm rounded bg-red-500 text-xs text-white"
                   @click="onDelete(user._id, user.email)"
