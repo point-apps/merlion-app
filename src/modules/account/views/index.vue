9d48b1fe24269c83be02b40a902ace163e55696f
Martien Dermawan Tanama <martiendt@gmail.com>
2024-12-17 14:44:09 +0700

    fix: size icon

--- src/modules/account/views/index.vue
+++ src/modules/account/views/index.vue
@@ -5,12 +5,12 @@
       <component :is="Breadcrumb" :breadcrumbs="[{ name: 'account' }]" />
     </div>
     <div class="grid grid-cols-2 gap-4 lg:grid-cols-4 xl:grid-cols-6">
-      <router-link to="/account/profile" class="flex flex-col space-y-1 rounded bg-white py-4 px-2 text-center shadow">
-        <fa-icon icon="fa-thin fa-user w-12 h-12" />
+      <router-link to="/account/profile" class="flex flex-col space-y-1 rounded bg-white px-2 py-4 text-center shadow">
+        <fa-icon icon="fa-thin fa-3x fa-user w-12 h-12" />
         <span>Profile</span>
       </router-link>
-      <router-link to="/account/security" class="flex flex-col space-y-1 rounded bg-white py-4 px-2 text-center shadow">
-        <fa-icon icon="fa-thin fa-lock w-12 h-12" />
+      <router-link to="/account/security" class="flex flex-col space-y-1 rounded bg-white px-2 py-4 text-center shadow">
+        <fa-icon icon="fa-thin fa-3x fa-lock w-12 h-12" />
         <span>Security</span>
       </router-link>
     </div>
