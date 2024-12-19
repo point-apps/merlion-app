4239a623ca11face669189bba00533177d808d21
Martien Dermawan Tanama <martiendt@gmail.com>
2024-12-19 13:12:10 +0700

    x

--- src/modules/strength-mapping/views/capture/show.vue
+++ src/modules/strength-mapping/views/capture/show.vue
@@ -27,10 +27,7 @@
         <label class="block space-y-1">
           <span class="font-semibold">Activity photos or videos</span>
           <div v-if="!capture.files" class="font-light italic">Not captured any photo or video</div>
-          <div
-            v-if="capture.files && capture.files?.[0]?.id != null"
-            class="flex flex-col space-x-3 lg:flex-row lg:flex-wrap"
-          >
+          <div v-if="capture.files && capture.files?.[0]?.id != null" class="flex flex-col space-x-3">
             <div
               v-for="(file, index) in capture.files"
               :key="index"
