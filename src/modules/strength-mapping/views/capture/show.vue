f831303a1c8319a0f5b152e894cdc03c0ef304ae
Martien Dermawan Tanama <martiendt@gmail.com>
2024-12-19 13:14:18 +0700

    x

--- src/modules/strength-mapping/views/capture/show.vue
+++ src/modules/strength-mapping/views/capture/show.vue
@@ -31,7 +31,7 @@
             <div
               v-for="(file, index) in capture.files"
               :key="index"
-              class="relative my-2 flex justify-center shadow dark:bg-slate-700"
+              class="relative my-2 flex max-h-[200px] min-h-[100px] flex-col justify-center shadow dark:bg-slate-700 lg:max-w-[200px]"
             >
               <iframe
                 v-if="file?.mimeType?.includes('video')"
