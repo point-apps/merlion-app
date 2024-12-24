39eaddbd78b5c32d906304a32f99878b62250eb3
Martien Dermawan Tanama <martiendt@gmail.com>
2024-12-24 11:46:38 +0700

    x

--- src/modules/strength-mapping/views/capture/edit.vue
+++ src/modules/strength-mapping/views/capture/edit.vue
@@ -176,7 +176,7 @@
             {{ error }}
           </p>
         </label>
-        <div class="rounded-lg border border-gray-300 bg-white p-4">
+        <div class="rounded-lg border border-gray-300 p-4 dark:border-gray-500">
           <div class="flex flex-row gap-2">
             <div>
               <p class="font-semibold">Define the Strength Map from the Activity!</p>
