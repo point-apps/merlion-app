7a8f5f0f623849dc56ffbdf3f1eed851d76531cb
Martien Dermawan Tanama <martiendt@gmail.com>
2025-01-10 21:34:55 +0700

    x

--- src/modules/strength-mapping/views/capture/edit.vue
+++ src/modules/strength-mapping/views/capture/edit.vue
@@ -117,11 +117,32 @@
             :key="index"
             class="relative my-2 flex max-h-[200px] min-h-[100px] flex-col justify-center shadow dark:bg-slate-700 lg:max-w-[200px]"
           >
-            <video v-if="file.mimeType.includes('video')" controls class="w-full">
+            <video v-if="!file.id && file.mimeType.includes('video')" controls class="w-full">
               <source :src="file.url" />
               Your browser does not support HTML5 video.
             </video>
-            <img v-else :src="file.url" alt="activity" class="relative max-h-[200px] lg:max-w-[200px]" />
+            <img
+              v-else-if="!file.id && !file.mimeType.includes('video')"
+              :src="file.url"
+              alt="activity"
+              class="relative max-h-[200px] lg:max-w-[200px]"
+            />
+            <iframe
+              v-if="file.id && file?.mimeType?.includes('video')"
+              :src="`https://drive.google.com/file/d/${file.id}/preview`"
+              frameborder="0"
+              height="360"
+              class="w-full"
+              allow="autoplay; encrypted-media"
+              allowfullscreen
+            >
+            </iframe>
+            <img
+              v-else-if="file.id && !file.mimeType.includes('video')"
+              :src="`https://drive.google.com/thumbnail?id=${file.id}&sz=w1200`"
+              alt="activity"
+              class="relative max-h-[200px] lg:max-w-[200px]"
+            />
             <button
               type="button"
               class="btn absolute right-2 top-2 rounded-full border-white bg-white px-2.5 py-1 opacity-50 shadow"
@@ -484,6 +505,9 @@ const removeCluster = (index: number) => {
 }
 
 const onRemoveFile = (index: number) => {
+  axios.post('/captures/' + route.params.id + '/delete-upload', {
+    id: form.value.files[index].id,
+  })
   form.value.files.splice(index, 1)
 }
 
