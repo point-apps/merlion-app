81c123d68f4cce81e31f8513a481cf1cf5cd8643
Martien Dermawan Tanama <martiendt@gmail.com>
2025-02-14 10:07:26 +0700

    gcf

--- src/modules/account/views/index.vue
+++ src/modules/account/views/index.vue
@@ -5,11 +5,17 @@
       <component :is="Breadcrumb" :breadcrumbs="[{ name: 'account' }]" />
     </div>
     <div class="grid grid-cols-2 gap-4 lg:grid-cols-4 xl:grid-cols-6">
-      <router-link to="/account/profile" class="flex flex-col space-y-1 rounded bg-white px-2 py-4 text-center shadow">
+      <router-link
+        to="/account/profile"
+        class="flex flex-col space-y-1 rounded bg-white px-2 py-4 text-center shadow dark:bg-slate-800"
+      >
         <fa-icon icon="fa-thin fa-3x fa-user w-12 h-12" />
         <span>Profile</span>
       </router-link>
-      <router-link to="/account/security" class="flex flex-col space-y-1 rounded bg-white px-2 py-4 text-center shadow">
+      <router-link
+        to="/account/security"
+        class="flex flex-col space-y-1 rounded bg-white px-2 py-4 text-center shadow dark:bg-slate-800"
+      >
         <fa-icon icon="fa-thin fa-3x fa-lock w-12 h-12" />
         <span>Security</span>
       </router-link>
