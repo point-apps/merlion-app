769ea22a4be0d709e63a119a186ff52c7480b657
Martien Dermawan Tanama <martiendt@gmail.com>
2025-09-02 12:30:53 +0700

    fix switch

--- src/components/switch.vue
+++ src/components/switch.vue
@@ -1,7 +1,11 @@
 <template>
   <label class="inline-flex items-center gap-2" :class="{ 'flex-row-reverse ': labelPosition === 'right' }">
     <span>{{ label }}</span>
-    <input v-model="modelValue" class="switch" type="checkbox" />
+    <div class="relative">
+      <input v-model="modelValue" class="switch" type="checkbox" />
+      <span v-if="modelValue" class="absolute left-2 top-0.5 text-white">Yes</span>
+      <span v-else class="absolute left-9 top-0.5 text-white">No</span>
+    </div>
   </label>
 </template>
 
