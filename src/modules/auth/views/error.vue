c104a486f995cfe8bf0b4f4fc8d6b355159fa257
Martien Dermawan Tanama <martiendt@gmail.com>
2025-09-22 10:38:52 +0700

    x

--- src/modules/auth/views/error.vue
+++ src/modules/auth/views/error.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-lg">This email account is not registered on Capture. Please contact Hyouman Customer Service at 
+  <div class="text-lg max-w-2xl text-center">This email account is not registered on Capture. Please contact Hyouman Customer Service at 
 +62 822-9999-3665 (Mon–Fri, 08:00–17:00 GMT+7) to request access</div>
 </template>
 
