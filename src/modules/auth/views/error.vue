fdde6bd3db518b0495c3ba13b049c50d3f763a0c
Martien Dermawan Tanama <martiendt@gmail.com>
2025-09-22 10:08:07 +0700

    x

--- src/modules/auth/views/error.vue
+++ src/modules/auth/views/error.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="text-lg">Your email has not been invited, please contact Hyouman to be added</div>
+  <div class="text-lg">This email account is not registered on Capture. Please contact Hyouman Customer Service at 
++62 822-9999-3665 (Mon–Fri, 08:00–17:00 GMT+7) to request access</div>
 </template>
 
 <script setup lang="ts">
